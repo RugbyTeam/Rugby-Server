7a60471f4c07ecbf5945d1f19077cc568f332ba0
Matthew Underwood <munde001@ucr.edu>
2015-02-10 20:58:44 -0800

    Creates a directory based on latest commit, and downloads the repositories .rugby.yml file to it

--- server.py
+++ server.py
@@ -1,15 +1,24 @@
 from flask import Flask, request, json
 from Rugby import Rugby
+import urllib
+import os
 
 rugby = Rugby()
 app = Flask(__name__)
-
+#this is the url that is used to download the rugby.yml file
+baseurl = "https://raw.githubusercontent.com/"
+yml = "/master/.rugby.yml"
 @app.route('/payload', methods=['POST'])
 def github_payload():
     payload = request.get_json()
     url = payload['repository']['clone_url']
+    dirname = payload['commits'][-1]['id'] 
+    os.mkdir(dirname)
+    ymlfile = urllib.URLopener()
+    url = baseurl+payload['repository']['full_name']+yml
     print url
-
+    ymlfile.retrieve(url, dirname + "/rugby.yaml")
+    print dirname
     if request.headers.get('X-Github-Event') == 'push':
         return "Received push"
     return "Received something..."
