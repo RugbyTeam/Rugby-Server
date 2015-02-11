f6d56bba20cca30572de5f012091376e677ad539
Matthew Underwood <munde001@ucr.edu>
2015-02-10 21:39:45 -0800

    now calls the parser on the downloaded yaml file.

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, json
-from Rugby import Rugby
+from rugby import Rugby
+from rugby import Parser
 import urllib
 import os
 
 rugby = Rugby()
 app = Flask(__name__)
 #this is the url that is used to download the rugby.yml file
-baseurl = "https://raw.githubusercontent.com/"
+base_url = "https://raw.githubusercontent.com/"
 yml = "/master/.rugby.yml"
 @app.route('/payload', methods=['POST'])
 def github_payload():
@@ -14,11 +15,17 @@ def github_payload():
     url = payload['repository']['clone_url']
     dirname = payload['commits'][-1]['id'] 
     os.mkdir(dirname)
-    ymlfile = urllib.URLopener()
-    url = baseurl+payload['repository']['full_name']+yml
+    yml_file = urllib.URLopener()
+    url = base_url+payload['repository']['full_name']+yml
     print url
-    ymlfile.retrieve(url, dirname + "/rugby.yaml")
+    yml_file.retrieve(url, dirname + "/rugby.yaml")
     print dirname
+    path_to_yml = dirname + "/rugby.yaml"
+    ##----not sure were to put this, Parsing##
+    p = Parser(path_to_yml)
+    print p.parsed_yml 
+    
+    ##---------
     if request.headers.get('X-Github-Event') == 'push':
         return "Received push"
     return "Received something..."
