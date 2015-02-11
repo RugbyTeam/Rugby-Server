2dfc2427694ecd3e89fb604736a770623bafee91
Brandon Huynh <yuki@mobilenet2-3-198-24.bulk.ucr.edu>
2015-02-11 13:26:33 -0800

    Removing parsing code from server. Pass .rugby.yml to Rugby module instead

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, json
 from rugby import Rugby
-from rugby import Parser
 import urllib
 import os
 
 # Initializing necessary objects
 app = Flask(__name__)
 rugby = Rugby(os.getcwd())
+
 #this is the url that is used to download the rugby.yml file
 base_url = "https://raw.githubusercontent.com/"
 yml = "/master/.rugby.yml"
@@ -18,32 +18,27 @@ Routing Methods
 @app.route('/payload', methods=['POST'])
 def github_payload():
     payload = request.get_json()
-    url = payload['repository']['clone_url']
-    dirname = payload['commits'][-1]['id'] 
-    os.mkdir(dirname)
-    yml_file = urllib.URLopener()
-    url = base_url+payload['repository']['full_name']+yml
-    print url
-    yml_file.retrieve(url, dirname + "/rugby.yaml")
-    print dirname
-    path_to_yml = dirname + "/rugby.yaml"
-    ##----not sure were to put this, Parsing##
-    p = Parser(path_to_yml)
-    print p.parsed_yml 
-    
-    ##---------
+    commit_id = payload['commits'][-1]['id']
+    clone_url = payload['repository']['clone_url']
+    config_url = base_url + payload['repository']['full_name'] + yml
+   
+    # We grab the .rugby.yml file from the repo and pass it to Rugby object
+    try:
+        yml_file = urllib.urlopen(config_url)
+    except URLError:
+        print "Rugby-Server URLError on config retrieval"
+        return "Config Retrieval Error", 500
+
     if request.headers.get('X-Github-Event') == 'push':
-        return "Received push"
-    return "Received something..."
+        rugby.up(commit_id, yml_file.read())
+
+    return "Received something...", 200
 
 @app.route('/')
 def status():
     #TODO: Parse HTML status page here
     html = "This is a status page..."
-    commit_id = "genericcommitID"
-    clone_url = "this/is/a/url"
-    rugby.up(commit_id, clone_url)
-    return html
+    return html, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
