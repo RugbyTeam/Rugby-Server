a4167155d6a9969d1b20b749d4338b55fd644427
omoman <mannysanchez998@gmail.com>
2015-02-12 13:34:52 -0800

    Rewrote server.py to use GithubRepo class

--- server.py
+++ server.py
@@ -1,39 +1,55 @@
+# internal
+from github_repo import GithubRepo
+import config
+
+# external
 from flask import Flask, request, json
 from rugby import Rugby
-import urllib
+
+# stdlib
 import os
 
 # Initializing necessary objects
 app = Flask(__name__)
-rugby = Rugby(os.getcwd())
-
-#this is the url that is used to download the rugby.yml file
-base_url = "https://raw.githubusercontent.com/"
-yml = "/master/.rugby.yml"
+rugby = Rugby(config.RUGBY_ROOT)
 
 """
 Routing Methods
-
 """
 @app.route('/payload', methods=['POST'])
 def github_payload():
+    
+    # We don't care about 'ping' events, we only care about 'push
+    # events
+    if request.headers.get('X-Github-Event') != 'push':
+        return "Not a push event", 417
+    
     payload = request.get_json()
-    commit_id = payload['commits'][-1]['id']
-    clone_url = payload['repository']['clone_url']
-    config_url = base_url + payload['repository']['full_name'] + yml
-    config_file = "/tmp/"+commit_id
-    # We grab the .rugby.yml file from the repo and pass it to Rugby object
     try:
-        yml_file = urllib.URLopener()
-        yml_file.retrieve(config_url, config_file)
-    except URLError:
-        print "Rugby-Server URLError on config retrieval"
-        return "Config Retrieval Error", 500
+        gh_repo = GithubRepo(payload)
+    except Exception as e:
+        return str(e), 200
 
-    if request.headers.get('X-Github-Event') == 'push':
-        rugby.up(commit_id, config_file)
+    # We don't care about any other branch other than the 
+    # 'default' branch
+    if not gh_repo.cur_branch_is_default():
+        return "Not default branch", 417
+    
+    # Copy over rugby conf from repo
+    conf_dest_file = os.path.join(config.RUGBY_TMP, gh_repo.commit_id);
+    try:
+        conf_url = gh_repo.conf_url
+        conf_data = requests.get(conf_url)
+    except Exception as e:
+        return "Failed to find .rugby.yml", 417
+    
+    with open(conf_dest_file, 'w') as f:
+        f.write(conf_data.text())
 
-    return "Received something...", 200
+    # Run Rugby
+    Rugby.up(gh_repo.commit_id, conf_dest_file)
+
+    return "Success", 200
 
 @app.route('/')
 def status():
