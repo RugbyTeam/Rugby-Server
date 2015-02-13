3cf18bebba1767dbbc7273cb524cd49584637263
Alex Gonzales <agonz056@ucr.edu>
2015-02-13 15:39:26 -0500

    migrated flask code to tornado

--- server.py
+++ server.py
@@ -3,59 +3,81 @@ from github_repo import GithubRepo
 import config
 
 # external
-from flask import Flask, request, json
 from rugby import Rugby
+import tornado.web
+import tornado.escape
+import tornado.gen
+import tornado.httpclient
 
 # stdlib
 import os
 
 # Initializing necessary objects
-app = Flask(__name__)
 rugby = Rugby(config.RUGBY_ROOT)
 
-"""
-Routing Methods
-"""
-@app.route('/payload', methods=['POST'])
-def github_payload():
-    
-    # We don't care about 'ping' events, we only care about 'push
-    # events
-    if request.headers.get('X-Github-Event') != 'push':
-        return "Not a push event", 417
-    
-    payload = request.get_json()
-    try:
-        gh_repo = GithubRepo(payload)
-    except Exception as e:
-        return str(e), 200
+class GitHubHookHandler(tornado.web.RequestHandler):
+    def prepare(self):
+        self.github_event = self.request.headers.get('X-Github-Event')
+        if self.request.body:
+            try:
+                self.payload = tornado.escape.json_decode(self.request.body)
+            except ValueError:
+                message = 'Unable to parse JSON.'
+                self.send_error(400, message=message)
+                return
 
-    # We don't care about any other branch other than the 
-    # 'default' branch
-    if not gh_repo.cur_branch_is_default():
-        return "Not default branch", 417
-    
-    # Copy over rugby conf from repo
-    conf_dest_file = os.path.join(config.RUGBY_TMP, gh_repo.commit_id);
-    try:
-        conf_url = gh_repo.conf_url
-        conf_data = requests.get(conf_url)
-    except Exception as e:
-        return "Failed to find .rugby.yml", 417
-    
-    with open(conf_dest_file, 'w') as f:
-        f.write(conf_data.text())
+    @tornado.gen.coroutine
+    def post(self):
+        # We don't care about 'ping' events, we only care about 'push' events
+        http_client = tornado.httpclient.AsyncHTTPClient()
 
-    # Run Rugby
-    Rugby.up(gh_repo.commit_id, conf_dest_file)
+        if self.github_event != 'push':
+            self.send_error(417, message="Not a push event.")
+            return
+        try:
+            gh_repo = GithubRepo(self.payload)
+        except Exception as e:
+            self.write(str(e))
+            self.finish()
+            return
+        # We don't care about any other branch other than the 'default' branch
+        if not gh_repo.cur_branch_is_default():
+            message = 'Not a default branch.'
+            self.send_error(417, message=message)
+            return
 
-    return "Success", 200
+        # Copy over rugby conf from repo
+        conf_dest_file = os.path.join(config.RUGBY_TMP, gh_repo.commit_id);
+        try:
+            config_url = gh_repo.config_url
+            config_data = yield http_client.fetch(config_url)
+        except Exception as e:
+            message = 'Failed to find .rugby.yml.' + str(e)
+            self.send_error(417, message=message)
+            return
 
-@app.route('/')
-def status():
-    #TODO: Parse HTML status page here
-    html = "This is a status page..."
-    return html, 200
+        with open(config_dest_file, 'w') as f:
+            f.write(config_data.body)
+
+        # Run Rugby
+        Rugby.up(gh_repo.commit_id, config_dest_file)
+
+        # Start logging task
+        self.write('Success')
+
+class StatusPageHandler(tornado.web.RequestHandler):
+    def get(self, commit_id):
+        self.write('This is a status page')
+        if commit_id != None:
+            self.write(commit_id)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    github_hook_route = tornado.web.url(r'/payload', GitHubHookHandler)
+    sha1_regex = r'/([0-9a-f]{5,40})?'
+    status_page_route = tornado.web.url(sha1_regex, StatusPageHandler)
+
+    routes = [github_hook_route, status_page_route]
+
+    app = tornado.web.Application(routes)
+    app.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
