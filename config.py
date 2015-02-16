f601fa2bfa8eec17b49143af7bf329329de9bb80
Alex Gonzales <agonz056@ucr.edu>
2015-02-16 17:50:49 -0500

    need to set enviornment variables

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import os
+
 # Web server constants
 RAW_GIT_URL_BASE = 'https://raw.githubusercontent.com'
 RUGBY_ROOT = '/opt/Rugby'
 RUGBY_CONF = '.rugby.yml'
 RUGBY_TMP = '/tmp'
+
+# Get environment variables for mail server
+GMAIL_USER = os.getenv('GMAIL_USER')
+GMAIL_PASSWD = os.getenv('GMAIL_PASSWD')
